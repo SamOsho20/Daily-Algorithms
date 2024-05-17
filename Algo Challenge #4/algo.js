6169c338f8275603055a4ebe07014b6ef27377ca
Sam_Osho <Sosho108@marian.edu>
2024-05-16 22:27:23 -0400

    Simple algo challenge #4 in js!

---
+++ Algo Challenge #4/algo.js
@@ -0,0 +1,18 @@
+
+
+// PRACTICING SIMPLE ALGO CHALLENGES 
+// 1.
+var x = [1,3,5]
+
+var y = x.length
+
+console.log(y)
+// output ---- 3 
+
+
+// 2.
+
+var x = [2,4,6,3,7]
+var y = x.length - 2
+console.log(x[y + 1])
+// output ---- 7
