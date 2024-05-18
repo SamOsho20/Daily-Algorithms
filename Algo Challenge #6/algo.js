8cf27cd61ecfe6292451a3036f3668a04c85567f
Sam_Osho <Sosho108@marian.edu>
2024-05-18 09:02:24 -0400

    Simple Algo Challenge #6 in js!

---
+++ Algo Challenge #6/algo.js
@@ -0,0 +1,25 @@
+// 1
+
+
+var x = [1,3,5,7]
+
+var y = [2,4,6]
+
+x[2] = y[x.length-3] + x[x[0] + y [0]]
+
+console.log(x)
+
+// OUTPUT ---> [1,3,11,7]
+
+
+
+// 2
+
+var x = [1,3,5,8]
+
+x[0] = x[x.length - 1]
+
+console.log(x)
+
+
+// output ----> [8,3,5,8]
